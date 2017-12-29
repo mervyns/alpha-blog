b177c2aa955424150a06754c00cd9233c50d195c
mervyn <mervyns@gmail.com>
2017-12-29 17:26:56 +0000

    added edit update show and indexx

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end    
   
   def create
     #render plain: params[:article].inspect
@@ -12,13 +20,24 @@ class ArticlesController < ApplicationController
     else 
       render 'new'
     end
+  end  
+  
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated."
+        redirect_to article_path(@article) 
+      else
+        render 'edit'
+      end
+    end
     
     def show
       @article = Article.find(params[:id])
     end
   
 
-  end
+  
 
   private
   def article_params
@@ -26,3 +45,4 @@ class ArticlesController < ApplicationController
   end
 
 end
+
