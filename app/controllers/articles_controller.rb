1bc8a712831cc3fdf25b73b8160a235e5e8a9a8d
mervyn <mervyns@gmail.com>
2017-12-29 17:02:34 +0000

    created show etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,20 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    render plain: params[:article].inspect
+    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "article was successfully saved"
+    redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
+    
+    def show
+      @article = Article.find(params[:id])
+    end
+  
+
   end
 
   private
