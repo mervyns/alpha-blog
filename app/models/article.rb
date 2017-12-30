251f2f058137b609e242d1344a224b56f9229535
mervyn <mervyns@gmail.com>
2017-12-30 17:18:12 +0000

    added debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+  before_save { self.email = email.downcase }
   validates :title, presence: true, length: { minimum: 3, maximum: 10}
   validates :descriptions, presence: true, length: {minimum: 10, maximum: 300}
   
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/i
+    validates :email, presence: true,
+            uniqueness: { case_sensitive: false},
+            format: { with: VALID_EMAIL_REGEX }
 end
