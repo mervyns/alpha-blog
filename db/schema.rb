a03f58a24918fdd479895ca0cc2a294691c2cb85
mervyn <mervyns@gmail.com>
2017-12-30 16:46:34 +0000

    created users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229154946) do
+ActiveRecord::Schema.define(version: 20171230163837) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "descriptions"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
